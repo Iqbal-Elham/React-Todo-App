3305c8f94c66e686857dc85425e09cbae25cc13c
iqbal-elham <iqbal.elham7@gmail.com>
2023-02-22 16:01:37 +0430

    Modify the logic comp

--- src/components/TodoLogic.js
+++ src/components/TodoLogic.js
@@ -1,6 +1,30 @@
+import InputTodo from "./InputTodo";
+import TodoList from "./TodoList";
+
 const TodosLogic = () => {
+    const todos = [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ];
+
     return (
-      <div>TodosLogic content</div>
+      <div> <h2>TodosLogic content</h2>
+        <InputTodo />
+        <TodoList todoProp={todos} />
+      </div>
     )
   }
   export default TodosLogic;
