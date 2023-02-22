87af0a44daa5672e4a5926163c5cfcad22cafc78
iqbal-elham <iqbal.elham7@gmail.com>
2023-02-22 22:05:34 +0430

    Add validation for the input

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,28 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const InputTodo = ({addTodoItem}) => {
-    const [ title, setTitle ] = useState('');
+const InputTodo = ({ addTodoItem }) => {
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleChange = (e) => {
-      setTitle(e.target.value);
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle("");
+      setMessage("");
+    } else {
+      setMessage("Please add a Todo");
     }
+  };
 
-    const handleAdd = (e) => {
-      e.preventDefault();
-      addTodoItem(title)
-      setTitle('');
-    }
-
-    return (
+  return (
+    <>
       <form onSubmit={handleAdd}>
-        <input type="text" 
-        placeholder="Add Todo..."
-        value={title}
-        onChange={handleChange}
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
         />
         <button>Add</button>
       </form>
-    );
-  };
-  export default InputTodo;
-  
+      <span>{message}</span>
+    </>
+  );
+};
+export default InputTodo;
