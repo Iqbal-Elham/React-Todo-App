7be9f323150660c0b6d8d2f34e909dc48be07b8a
iqbal-elham <iqbal.elham7@gmail.com>
2023-02-22 15:16:27 +0430

    Add to strict mode

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import TodoApp from './components/TodoApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <TodoApp />
+  </React.StrictMode>
 );
 
 
